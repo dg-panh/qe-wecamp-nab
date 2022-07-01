7baf72d2d292b7e1abb3720daf51be6957d2ac1e
panhpanh <dangphuonganh200136@gmail.com>
2022-07-01 18:07:06 +0700

    Cypress demo

---
+++ cypressWeek2/cypress/e2e/spec.cy.js
@@ -0,0 +1,14 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://demo.guru99.com/test/newtours/');
+    cy.wait(10000);
+    cy.get("input[name='userName']").click();
+    cy.get("input[name='userName']").type('tutorial');
+    cy.get("input[name='password']").click();
+    cy.get("input[name='password']").type('tutorial');
+    cy.get("input[name='submit']").click();
+    cy.wait(1000);
+    cy.get("h3").should('be.visible').and('have.text', 'Login Successfully');
+    cy.get("h3").contains('Login Successfully');
+  })
+})
